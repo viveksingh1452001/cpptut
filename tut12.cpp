ccb7670eed917f3e684d746d825b1ea56d8243cc
viveksingh1452001 <vs2467@srmist.edu.in>
2021-09-04 14:57:25 +0530

    ...

--- tut12.cpp
+++ tut12.cpp
@@ -100,22 +100,38 @@
 
 //*****************palindrome****************************
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int sum=0,r,i,temp;
+//     cout<<"enter the number: ";
+//     cin>>i;
+//     temp=i;
+//     while(i>0){
+//         r=i%10;
+//         sum=(sum*10)+r;
+//         i=i/10;
+//     }
+//     if(sum==temp)
+//     cout<<"the number is palindrome";
+//     else
+//     cout<<"the number is not palindrome";
+//     return 0;
+// }
+
+//******************factorial***************
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    int sum=0,r,i,temp;
-    cout<<"enter the number: ";
-    cin>>i;
-    temp=i;
-    while(i>0){
-        r=i%10;
-        sum=(sum*10)+r;
-        i=i/10;
+    int a,i,j=1;
+    cout<<"enter the value: ";
+    cin>>a;
+    for(i=1;i<=a;i++){
+        j=j*i;
     }
-    if(sum==temp)
-    cout<<"the number is palindrome";
-    else
-    cout<<"the number is not palindrome";
+    cout<<j;
     return 0;
 }
